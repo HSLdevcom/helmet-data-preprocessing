97742c1b113013f4e65234f709a5e2bca3ea542f
Johanna Piipponen <johanna.piipponen@strafica.fi>
2019-05-08 14:12:45 +0300

    Translate `aluejako` to `zones`

--- input/zones.R
+++ input/zones.R
@@ -1,16 +1,16 @@
 # -*- coding: windows-1252-dos -*-
 library(strafica)
 
-aluejako = read.shape(ancfile("data/raw/sijoittelualueet2019/sijoittelualueet2019.shp"),
+zones = read.shape(ancfile("data/raw/sijoittelualueet2019/sijoittelualueet2019.shp"),
                       encoding="utf-8")
-aluejako = sp.to.polys(aluejako, data.only=TRUE)
-aluejako = pick(aluejako, sij2019, kela)
-aluejako = rename(aluejako, sij2019=zone_orig, kela=municipality)
-aluejako$municipality = as.integer(aluejako$municipality)
-aluejako = arrange(aluejako, zone_orig)
-aluejako$zone = rows.along(aluejako)
+zones = sp.to.polys(zones, data.only=TRUE)
+zones = pick(zones, sij2019, kela)
+zones = rename(zones, sij2019=zone_orig, kela=municipality)
+zones$municipality = as.integer(zones$municipality)
+zones = arrange(zones, zone_orig)
+zones$zone = rows.along(zones)
 
-aluejako$cbd = ifelse(aluejako$zone_orig %in% c(101:999, 1531, 1532), TRUE, FALSE)
-aluejako$suburb = ifelse(aluejako$zone_orig %in% c(1000:1999, 244, 367, 368, 1570), TRUE, FALSE)
+zones$cbd = ifelse(zones$zone_orig %in% c(101:999, 1531, 1532), TRUE, FALSE)
+zones$suburb = ifelse(zones$zone_orig %in% c(1000:1999, 244, 367, 368, 1570), TRUE, FALSE)
 
-write.csv2(aluejako, file="zones.csv", row.names=FALSE)
+write.csv2(zones, file="zones.csv", row.names=FALSE)
