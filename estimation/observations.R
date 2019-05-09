31660be1bcd49c95fdfc68f59c6ab0b36de53ac5
Johanna Piipponen <johanna.piipponen@strafica.fi>
2019-05-09 18:23:15 +0300

    Generate observations from a list of files

--- estimation/observations.R
+++ estimation/observations.R
@@ -5,83 +5,81 @@ library(readxl)
 zones = load1("zones.RData")
 background = load1("background.RData")
 
-observations = list()
-
-# Tour
+input = read.delims("input.txt")
 
 message("Formatting tour data...")
 
-tours1 = read.csv2(ancfile("tours/output/tours-heha.csv"),
-                   stringsAsFactors=FALSE)
-tours2 = read.csv2(ancfile("tours/output/tours-hlt.csv"),
-                   stringsAsFactors=FALSE)
-tours = rbind_list(tours1, tours2)
-
-df = data.frame(pid=tours$pid)
-df$mode = tours$mode
-df$ttype = NA
-m = grepl("^(1 - 2)", tours$tour_type, perl=TRUE)
-df$ttype[m] = 1
-m = grepl("^(1 - 3)", tours$tour_type, perl=TRUE)
-df$ttype[m] = 2
-m = grepl("^(1 - 4)", tours$tour_type, perl=TRUE)
-df$ttype[m] = 3
-m = grepl("^(1 - 5)", tours$tour_type, perl=TRUE)
-df$ttype[m] = 4
-m = grepl("^1$|^(1 - [16])", tours$tour_type, perl=TRUE)
-df$ttype[m] = 5
-m = grepl("^2$|^(2 - [123456])", tours$tour_type, perl=TRUE)
-df$ttype[m] = 6
-m = is.na(df$ttype)
-df$ttype[m] = 7
-df$other_destinations = ifelse(rowSums(tours[, grepl("^visits_t", colnames(tours), perl=TRUE)]) > 2, 1, 0)
-df$closed = ifelse(tours$closed, 1, 2)
-
-m = match(tours$zone_origin, zones$zone_orig)
-df$izone = zones$zone[m]
-df$izone_cbd = ifelse(zones$cbd[m]==1, 1, 0)
-df$izone_population_density = zones$population_density[m]
-df$izone_housing = zones$housing[m]
-df$izone_parking_fee_other = zones$parking_fee_other[m]
-df$izone_cars_per_people = zones$cars_per_people[m]
-
-m = match(tours$zone_destination, zones$zone_orig)
-df$jzone = zones$zone[m]
-
-df$ipeak = apply.breaks(tours$itime_origin,
-                           class=c("morning","afternoon"),
-                           c("06:00:00","15:00:00"),
-                           c("08:59:59","17:59:59"))
-m = which(is.na(df$ipeak))
-df$ipeak[m] = "other"
-m = which(tours$itime_origin == "")
-df$ipeak[m] = NA
-
-df$jpeak = apply.breaks(tours$itime_destination,
-                           class=c("morning","afternoon"),
-                           c("06:00:00","15:00:00"),
-                           c("08:59:59","17:59:59"))
-m = which(is.na(df$jpeak))
-df$jpeak[m] = "other"
-m = which(tours$itime_destination == "")
-df$jpeak[m] = NA
-
-# If tour visits only one place and returns to it, do not interpret a return
-# time slot.
-m = which(rowSums(tours[,grep("^visits_t[0-9]+", colnames(tours))]) == 1)
-df$jpeak[m] = NA
-
-df$mtype = ifelse(df$ttype==1,
-                  "hbwork",
-                  ifelse(df$ttype %in% c(2,3,4,5),
-                         "hbother",
-                         "nhb"))
-
-observations = rbind_list(observations, df)
-
-
-observations = leftjoin(observations, background)
-
-observations = downclass(observations)
-check.na(observations)
-save(observations, file="observations.RData")
+for (i in rows.along(input)) {
+    
+    message(sprintf("%d/%d: %s", i, nrow(input), input$file[i]))
+    tours = load1(input$file[i])
+    
+    observations = data.frame(pid=tours$pid)
+    observations$mode = tours$mode
+    observations$ttype = NA
+    m = grepl("^(1 - 2)", tours$tour_type, perl=TRUE)
+    observations$ttype[m] = 1
+    m = grepl("^(1 - 3)", tours$tour_type, perl=TRUE)
+    observations$ttype[m] = 2
+    m = grepl("^(1 - 4)", tours$tour_type, perl=TRUE)
+    observations$ttype[m] = 3
+    m = grepl("^(1 - 5)", tours$tour_type, perl=TRUE)
+    observations$ttype[m] = 4
+    m = grepl("^1$|^(1 - [16])", tours$tour_type, perl=TRUE)
+    observations$ttype[m] = 5
+    m = grepl("^2$|^(2 - [123456])", tours$tour_type, perl=TRUE)
+    observations$ttype[m] = 6
+    m = is.na(observations$ttype)
+    observations$ttype[m] = 7
+    observations$other_destinations = ifelse(rowSums(tours[, grepl("^visits_t", colnames(tours), perl=TRUE)]) > 2, 1, 0)
+    observations$closed = ifelse(tours$closed, 1, 2)
+    
+    m = match(tours$zone_origin, zones$zone_orig)
+    observations$izone = zones$zone[m]
+    observations$izone_cbd = ifelse(zones$cbd[m]==1, 1, 0)
+    observations$izone_population_density = zones$population_density[m]
+    observations$izone_housing = zones$housing[m]
+    observations$izone_parking_fee_other = zones$parking_fee_other[m]
+    observations$izone_cars_per_people = zones$cars_per_people[m]
+    
+    m = match(tours$zone_destination, zones$zone_orig)
+    observations$jzone = zones$zone[m]
+    
+    observations$ipeak = apply.breaks(tours$itime_origin,
+                            class=c("morning","afternoon"),
+                            c("06:00:00","15:00:00"),
+                            c("08:59:59","17:59:59"))
+    m = which(is.na(observations$ipeak))
+    observations$ipeak[m] = "other"
+    m = which(tours$itime_origin == "")
+    observations$ipeak[m] = NA
+    
+    observations$jpeak = apply.breaks(tours$itime_destination,
+                            class=c("morning","afternoon"),
+                            c("06:00:00","15:00:00"),
+                            c("08:59:59","17:59:59"))
+    m = which(is.na(observations$jpeak))
+    observations$jpeak[m] = "other"
+    m = which(tours$itime_destination == "")
+    observations$jpeak[m] = NA
+    
+    # If tour visits only one place and returns to it, do not interpret a return
+    # time slot.
+    m = which(rowSums(tours[,grep("^visits_t[0-9]+", colnames(tours))]) == 1)
+    observations$jpeak[m] = NA
+    
+    observations$mtype = ifelse(observations$ttype==1,
+                      "hbwork",
+                      ifelse(observations$ttype %in% c(2,3,4,5),
+                             "hbother",
+                             "nhb"))
+    
+    # Add background information
+    observations = leftjoin(observations, background)
+    
+    # Output
+    observations = downclass(observations)
+    check.na(observations)
+    fname = sprintf("observations-%s.RData", input$name[i])
+    save(observations, file=fname)
+}
