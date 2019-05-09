64606531ebb0b28eab947fd1bac66ad7d2683be9
Johanna Piipponen <johanna.piipponen@strafica.fi>
2019-05-09 18:48:41 +0300

    Prepare auxiliary data first and then prepare estimation data

--- estimation/alternatives.R
+++ estimation/alternatives.R
@@ -16,28 +16,6 @@ check_class = function(x, exclude=NA) {
 }
 
 
-# Subset data to prevent memory running out
-alternatives = load1("observations.RData")
-
-# From which matrix travel times and lengths are read from?
-alternatives$aux_ttime_bicycle = sprintf("ttime_bicycle_%d", alternatives$year)
-alternatives$aux_length_bicycle_separate_cycleway = sprintf("length_bicycle_separate_cycleway_%d", alternatives$year)
-alternatives$aux_length_bicycle_adjacent_cycleway = sprintf("length_bicycle_adjacent_cycleway_%d", alternatives$year)
-alternatives$aux_length_bicycle_mixed_traffic = sprintf("length_bicycle_mixed_traffic_%d", alternatives$year)
-
-alternatives$aux_ttime_transit = sprintf("ttime_transit_%d_%s", alternatives$year, alternatives$mtype)
-alternatives$aux_cost_transit_work = sprintf("cost_transit_work_%d", alternatives$year)
-alternatives$aux_cost_transit_other = sprintf("cost_transit_other_%d", alternatives$year)
-
-alternatives$aux_ttime_car = sprintf("ttime_car_%d_%s", alternatives$year, alternatives$mtype)
-alternatives$aux_cost_car = sprintf("cost_car_%d_%s", alternatives$year, alternatives$mtype)
-
-alternatives$aux_ttime_pedestrian = sprintf("ttime_pedestrian_%d", alternatives$year)
-alternatives$aux_length_pedestrian = sprintf("length_pedestrian_%d", alternatives$year)
-
-matrices_needed = unique(unlist(alternatives[, grepl("^aux_", names(alternatives))]))
-
-
 # Load input files
 message("Loading input files...")
 time.start = Sys.time()
@@ -195,29 +173,45 @@ write_estimation_data = function(alternatives,
 }
 
 
-columns = read.delims("order-peripheral.txt")
-columns$column = sprintf("^%s$", columns$column)
-
-
-alternatives_peripheral = subset(alternatives,
-                                 model == 2)
-data_columns = write_estimation_data(alternatives=alternatives_peripheral,
-                                     batch_size=100,
-                                     model_name="peripheral",
-                                     row=row,
-                                     matrix_list=matrix_list,
-                                     columns=columns)
-message("Writing column names...")
-writeLines(data_columns, "alternatives-columns-peripheral.txt")
-
-
-# alternatives_metropolitan = subset(alternatives,
-#                                    model == 1)
-# data_columns = write_estimation_data(alternatives=alternatives_metropolitan,
-#                                      batch_size=100,
-#                                      model_name="metropolitan",
-#                                      row=row,
-#                                      matrix_list=matrix_list,
-#                                      columns=columns)
-# message("Writing column names...")
-# writeLines(data_columns, "alternatives-columns-metropolitan.txt")
+input = read.delims("input.txt")
+for (i in rows.along(input)) {
+    
+    fname = sprintf("observations-%s.RData", input$name[i])
+    message(sprintf("%d/%d: %s", i, nrow(input), fname)
+    alternatives = load1(fname)
+    
+    # From which matrix travel times and lengths are read from?
+    alternatives$aux_ttime_bicycle = sprintf("ttime_bicycle_%d", alternatives$year)
+    alternatives$aux_length_bicycle_separate_cycleway = sprintf("length_bicycle_separate_cycleway_%d", alternatives$year)
+    alternatives$aux_length_bicycle_adjacent_cycleway = sprintf("length_bicycle_adjacent_cycleway_%d", alternatives$year)
+    alternatives$aux_length_bicycle_mixed_traffic = sprintf("length_bicycle_mixed_traffic_%d", alternatives$year)
+    
+    alternatives$aux_ttime_transit = sprintf("ttime_transit_%d_%s", alternatives$year, alternatives$mtype)
+    alternatives$aux_cost_transit_work = sprintf("cost_transit_work_%d", alternatives$year)
+    alternatives$aux_cost_transit_other = sprintf("cost_transit_other_%d", alternatives$year)
+    
+    alternatives$aux_ttime_car = sprintf("ttime_car_%d_%s", alternatives$year, alternatives$mtype)
+    alternatives$aux_cost_car = sprintf("cost_car_%d_%s", alternatives$year, alternatives$mtype)
+    
+    alternatives$aux_ttime_pedestrian = sprintf("ttime_pedestrian_%d", alternatives$year)
+    alternatives$aux_length_pedestrian = sprintf("length_pedestrian_%d", alternatives$year)
+    
+    matrices_needed = unique(unlist(alternatives[, grepl("^aux_", names(alternatives))]))
+    
+    fname = sprintf("order-%s.txt", input$name[i])
+    columns = read.delims(fname)
+    columns$column = sprintf("^%s$", columns$column)
+    
+    message("Writing estimation data...")
+    data_columns = write_estimation_data(alternatives=alternatives,
+                                         batch_size=100,
+                                         model_name=input$name[i],
+                                         row=row,
+                                         matrix_list=matrix_list,
+                                         columns=columns)
+    
+    message("Writing column names...")
+    fname = sprintf("alternatives-columns-%s.txt", input$name[i])
+    writeLines(data_columns, fname)
+    
+}
