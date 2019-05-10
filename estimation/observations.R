1d4a24e86d6f3b8b158a9831bf9ed0e5271a74f6
Johanna Piipponen <johanna.piipponen@strafica.fi>
2019-05-10 13:50:36 +0300

    Get tour classes via function
    Also fixed a column reference which gave constructed tours wrong
    classes.

--- estimation/observations.R
+++ estimation/observations.R
@@ -2,6 +2,37 @@
 library(strafica)
 library(readxl)
 
+
+#' Get tour classes from tour types.
+#' 
+#' @param x A character vector of tour types (\code{tour_type}) as defined in \code{tours/output} files.
+#' @param y A logical vector of whether tour is constructed or not.
+#' @return An integer vector of which tour class does the tour belong to.
+get_ttype = function(x, y) {
+    stopifnot(is.character(x))
+    stopifnot(is.logical(y))
+    classes = rep(NA, times=length(x))
+    # Seven classes for survey tours
+    patterns = c("^(1 - 2)",
+                 "^(1 - 3)",
+                 "^(1 - 4)",
+                 "^(1 - 5)",
+                 "^1$|^(1 - [16])",
+                 "^2$|^(2 - [123456])")
+    for (i in seq(patterns)) {
+        m = grepl(patterns[i], x, perl=TRUE)
+        classes[m] = i
+    }
+    m = is.na(classes)
+    classes[m] = 7
+    # Two classes for constructed tours
+    m = (y & classes == 1)
+    classes[m] = 8
+    m = (y & classes != 8)
+    classes[m] = 9
+    return(classes)
+}
+
 zones = load1("zones.RData")
 background = load1("background.RData")
 
@@ -16,26 +47,8 @@ for (i in rows.along(input)) {
     
     observations = data.frame(pid=tours$pid)
     observations$mode = tours$mode
-    observations$ttype = NA
-    m = grepl("^(1 - 2)", tours$tour_type, perl=TRUE)
-    observations$ttype[m] = 1
-    m = grepl("^(1 - 3)", tours$tour_type, perl=TRUE)
-    observations$ttype[m] = 2
-    m = grepl("^(1 - 4)", tours$tour_type, perl=TRUE)
-    observations$ttype[m] = 3
-    m = grepl("^(1 - 5)", tours$tour_type, perl=TRUE)
-    observations$ttype[m] = 4
-    m = grepl("^1$|^(1 - [16])", tours$tour_type, perl=TRUE)
-    observations$ttype[m] = 5
-    m = grepl("^2$|^(2 - [123456])", tours$tour_type, perl=TRUE)
-    observations$ttype[m] = 6
-    m = is.na(observations$ttype)
-    observations$ttype[m] = 7
-    m = (observations$constructed & observations$ttype == 1)
-    observations$ttype[m] = 8
-    m = (observations$constructed & observations$ttype != 8)
-    observations$ttype[m] = 9
-    
+    observations$ttype = get_ttype(tours$tour_type,
+                                   tours$constructed)
     observations$other_destinations = ifelse(rowSums(tours[, grepl("^visits_t", colnames(tours), perl=TRUE)]) > 2, 1, 0)
     observations$closed = ifelse(tours$closed, 1, 2)
     
