aae7009537e2afb11e0ab43424fe5222990c00de
Johanna Piipponen <johanna.piipponen@strafica.fi>
2019-05-22 13:39:54 +0300

    Add readxl dependency

--- install-dependencies.R
+++ install-dependencies.R
@@ -1,2 +1,5 @@
-
+# Strafica dependencies
 install.packages(c('data.table', 'ggplot2', 'scales', 'DBI', 'plyr', 'png', 'rgdal', 'rgeos', 'sp', 'magrittr'))
+# helmet-data-preprocessing -specific dependencies
+install.packages(c('readxl'))
+
