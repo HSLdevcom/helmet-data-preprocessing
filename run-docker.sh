46b6ca033983fd2370f0d42f1fca3b5806fda21f
Markus Paasovaara <markus.paasovaara@futurice.com>
2019-05-22 09:54:40 +0300

    Reformatted run script

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Mount external dependencies (proprietary libraries and data)
-docker run -it --rm -v ${PWD}/strafica:/helmet-data-preprocessing/strafica helmet-data-preprocessing
+docker run -it --rm \
+  -v ${PWD}/strafica:/helmet-data-preprocessing/strafica \
+  helmet-data-preprocessing
