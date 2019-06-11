e735bf8b865c9beaee76531512e50d3dcf137934
Kimmo Tuukkanen <kimmo.tuukkanen@futurice.com>
2019-06-11 16:21:15 +0300

    fixed input/output paths and ignores

--- estimation/alternatives/README.md
+++
@@ -1,3 +0,0 @@
-# estimation/alternatives
-
-This folder contains estimation data that serves as input to [helmet-estimation](https://github.com/HSLdevcom/helmet-estimation). Estimation data files appear after `estimation/alternatives.R` is run.
--- run-docker.sh
+++ run-docker.sh
@@ -4,15 +4,15 @@ if (( $# < 1 )); then
   exit 1
 fi
 
-RAW_DATA_FOLDER=${PWD}/$1
-echo "Mounting and reading raw data from local dir ${RAW_DATA_FOLDER}"
+INPUT_FOLDER=${PWD}/$1
+echo "Mounting and reading input data from local dir ${INPUT_FOLDER}"
 
 OUTPUT_FOLDER=${PWD}/${2:-'estimation/alternatives'}
 echo "Mounting and writing output to local dir ${OUTPUT_FOLDER}"
 
 # Mount external dependencies (proprietary libraries and data)
 docker run -it --rm \
-  -v ${RAW_DATA_FOLDER}:/helmet-data-preprocessing/data/raw/ \
-  -v ${OUTPUT_FOLDER}:/helmet-data-preprocessing/tours/output/ \
+  -v ${INPUT_FOLDER}:/helmet-data-preprocessing/data/raw/ \
+  -v ${OUTPUT_FOLDER}:/helmet-data-preprocessing/output/ \
   -v ${PWD}/strafica:/helmet-data-preprocessing/strafica \
   helmet-data-preprocessing
