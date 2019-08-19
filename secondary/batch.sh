e509083f93ca30772e062650336692f8b8864cfe
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-08-19 15:21:31 +0300

    Update batch.sh

--- secondary/batch.sh
+++ secondary/batch.sh
@@ -3,6 +3,7 @@
 (cd alternatives/ && rm *.txt)
 rm *.RData *.csv
 
+Rscript --quiet --no-save --encoding=CP1252 tours.R
 Rscript --quiet --no-save --encoding=CP1252 observations.R
 Rscript --quiet --no-save --encoding=CP1252 average.R
 Rscript --quiet --no-save --encoding=CP1252 alternatives.R
