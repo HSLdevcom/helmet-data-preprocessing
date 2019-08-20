c37abe6e66b24b6882e48ee8063e7d292a3cd3a1
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-08-20 09:10:33 +0300

    Fix routines

--- batch.sh
+++ batch.sh
@@ -5,6 +5,7 @@
 pipenv run python ./tours/main.py input-config-heha.json
 pipenv run python ./tours/main.py input-config-hlt.json
 (cd estimation/ && sh batch.sh)
+(cd secondary/ && sh batch.sh)
 
 cat estimation/alternatives/alternatives-peripheral-1_100.txt | fold -w 180 -s > output/estimation/alternatives-peripheral-test.txt
 cat estimation/alternatives/alternatives-constructed-peripheral-1_100.txt | fold -w 180 -s > output/estimation/alternatives-constructed-peripheral-test.txt
@@ -13,7 +14,6 @@ cat estimation/alternatives/alternatives-wss-metropolitan-1_100.txt | fold -w 18
 cat estimation/alternatives/alternatives-spb-metropolitan-1_100.txt | fold -w 180 -s > output/estimation/alternatives-spb-metropolitan-test.txt
 cat estimation/alternatives/alternatives-other-metropolitan-1_100.txt | fold -w 180 -s > output/estimation/alternatives-other-metropolitan-test.txt
 cat estimation/alternatives/alternatives-wbo-metropolitan-1_100.txt | fold -w 180 -s > output/estimation/alternatives-wbo-metropolitan-test.txt
-cat estimation/alternatives/alternatives-secondary-metropolitan-1_100.txt | fold -w 180 -s > output/estimation/alternatives-secondary-metropolitan-test.txt
 
 cat estimation/alternatives/alternatives-peripheral-*.txt | fold -w 180 -s > output/estimation/alternatives-peripheral.txt
 cat estimation/alternatives/alternatives-constructed-peripheral-*.txt | fold -w 180 -s > output/estimation/alternatives-constructed-peripheral.txt
@@ -22,5 +22,6 @@ cat estimation/alternatives/alternatives-wss-metropolitan-*.txt | fold -w 180 -s
 cat estimation/alternatives/alternatives-spb-metropolitan-*.txt | fold -w 180 -s > output/estimation/alternatives-spb-metropolitan.txt
 cat estimation/alternatives/alternatives-other-metropolitan-*.txt | fold -w 180 -s > output/estimation/alternatives-other-metropolitan.txt
 cat estimation/alternatives/alternatives-wbo-metropolitan-*.txt | fold -w 180 -s > output/estimation/alternatives-wbo-metropolitan.txt
-cat estimation/alternatives/alternatives-secondary-metropolitan-*.txt | fold -w 180 -s > output/estimation/alternatives-secondary-metropolitan.txt
 
+cat secondary/alternatives/alternatives-metropolitan-secondary-1_100.txt | fold -w 180 -s > output/estimation/alternatives-metropolitan-secondary-test.txt
+cat secondary/alternatives/alternatives-metropolitan-secondary-*.txt | fold -w 180 -s > output/estimation/alternatives-metropolitan-secondary.txt
---
+++ secondary/alternatives/README.md
@@ -0,0 +1,4 @@
+# secondary/alternatives
+
+This folder contains temporary output files after `secondary/alternatives.R` is
+run.
