e6d1c40cd4063de5122da308b951ef6a3a2fc5cc
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-09-02 16:52:50 +0300

    Fix filetype

--- demand/no_of_tours_by_type.R
+++ demand/no_of_tours_by_type.R
@@ -105,4 +105,4 @@ all$name = unlist(mclapply(rows.along(all), function(i) {
               .rep("M", times=all$class_m[i]))
     paste(types, collapse=" - ")
 }))
-write.csv2(all, file="no_of_tours_by_type.txt", row.names=FALSE)
+write.csv2(all, file="no_of_tours_by_type.csv", row.names=FALSE)
