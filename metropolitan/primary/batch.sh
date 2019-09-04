c5bf387914f14a81780219c865589b0be5ee37fe
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-09-04 14:49:01 +0300

    Fix filenames

--- metropolitan/primary/batch.sh
+++ metropolitan/primary/batch.sh
@@ -4,9 +4,7 @@
 rm *.RData *.csv
 
 Rscript --quiet --no-save --encoding=CP1252 background.R
-
 Rscript --quiet --no-save --encoding=CP1252 tours.R
-Rscript --quiet --no-save --encoding=CP1252 constructed.R
 Rscript --quiet --no-save --encoding=CP1252 observations.R
 Rscript --quiet --no-save --encoding=CP1252 average.R
 Rscript --quiet --no-save --encoding=CP1252 alternatives.R
