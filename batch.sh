bce5980e3ec6834ab2bf56eed5de08f71984f6aa
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-09-05 10:17:31 +0300

    Fix filepaths in concatenating and copying data

--- batch.sh
+++ batch.sh
@@ -13,36 +13,41 @@ pipenv run python ./tours/main.py input-config-hlt.json
 (cd metropolitan/primary/ && sh batch.sh)
 (cd metropolitan/constructed/ && sh batch.sh)
 (cd metropolitan/secondary/ && sh batch.sh)
-(cd peripehral/primary/ && sh batch.sh)
-(cd peripehral/constructed/ && sh batch.sh)
+(cd peripheral/primary/ && sh batch.sh)
+(cd peripheral/constructed/ && sh batch.sh)
 
 cp -ruv tours/tours-*.csv $OUTPUT/
 
-cp -ruv metropolitan/primary/alternatives/columns-*.txt $OUTPUT/columns-metropolitan.txt
-cp -ruv metropolitan/constructed/alternatives/columns-*.txt $OUTPUT/columns-constructed-metropolitan.txt
-cp -ruv metropolitan/secondary/alternatives/columns-*.txt $OUTPUT/columns-metropolitan-secondary.txt
+cp -ruv metropolitan/primary/alternatives/columns-wss.txt $OUTPUT/columns-wss-metropolitan.txt
+cp -ruv metropolitan/primary/alternatives/columns-wbo.txt $OUTPUT/columns-wbo-metropolitan.txt
+cp -ruv metropolitan/primary/alternatives/columns-other.txt $OUTPUT/columns-other-metropolitan.txt
+cp -ruv metropolitan/primary/alternatives/columns-spb.txt $OUTPUT/columns-spb-metropolitan.txt
 
-cp -ruv peripheral/primary/alternatives/columns-*.txt $OUTPUT/columns-peripheral.txt
-cp -ruv peripheral/constructed/alternatives/columns-*.txt $OUTPUT/columns-constructed-peripheral.txt
+cp -ruv metropolitan/constructed/alternatives/columns.txt $OUTPUT/columns-constructed-metropolitan.txt
+cp -ruv metropolitan/secondary/alternatives/columns.txt $OUTPUT/columns-metropolitan-secondary.txt
 
-cat metropolitan/primary/alternatives/alternatives--1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-peripheral-test.txt
-cat metropolitan/primary/alternatives/alternatives-constructed-peripheral-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-peripheral-test.txt
-cat metropolitan/primary/alternatives/alternatives-constructed-metropolitan-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-metropolitan-test.txt
+cp -ruv peripheral/primary/alternatives/columns.txt $OUTPUT/columns-peripheral.txt
+cp -ruv peripheral/constructed/alternatives/columns.txt $OUTPUT/columns-constructed-peripheral.txt
 
-cat metropolitan/primary/alternatives/alternatives-wss-metropolitan-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-wss-metropolitan-test.txt
-cat metropolitan/primary/alternatives/alternatives-spb-metropolitan-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-spb-metropolitan-test.txt
-cat metropolitan/primary/alternatives/alternatives-other-metropolitan-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-other-metropolitan-test.txt
-cat metropolitan/primary/alternatives/alternatives-wbo-metropolitan-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-wbo-metropolitan-test.txt
+cat peripheral/primary/alternatives/alternatives--1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-peripheral-test.txt
+cat peripheral/constructed/alternatives/alternatives--1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-peripheral-test.txt
 
-cat metropolitan/primary/alternatives/alternatives-peripheral-*.txt | fold -w 180 -s > $OUTPUT/alternatives-peripheral.txt
-cat metropolitan/primary/alternatives/alternatives-constructed-peripheral-*.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-peripheral.txt
-cat metropolitan/primary/alternatives/alternatives-constructed-metropolitan-*.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-metropolitan.txt
-cat metropolitan/primary/alternatives/alternatives-wss-metropolitan-*.txt | fold -w 180 -s > $OUTPUT/alternatives-wss-metropolitan.txt
-cat metropolitan/primary/alternatives/alternatives-spb-metropolitan-*.txt | fold -w 180 -s > $OUTPUT/alternatives-spb-metropolitan.txt
-cat metropolitan/primary/alternatives/alternatives-other-metropolitan-*.txt | fold -w 180 -s > $OUTPUT/alternatives-other-metropolitan.txt
-cat metropolitan/primary/alternatives/alternatives-wbo-metropolitan-*.txt | fold -w 180 -s > $OUTPUT/alternatives-wbo-metropolitan.txt
+cat metropolitan/constructed/alternatives/alternatives--1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-metropolitan-test.txt
 
-cp -ruv secondary/alternatives/columns-*.txt $OUTPUT/
+cat metropolitan/primary/alternatives/alternatives-wss-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-wss-metropolitan-test.txt
+cat metropolitan/primary/alternatives/alternatives-spb-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-spb-metropolitan-test.txt
+cat metropolitan/primary/alternatives/alternatives-other-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-other-metropolitan-test.txt
+cat metropolitan/primary/alternatives/alternatives-wbo-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-wbo-metropolitan-test.txt
 
-cat secondary/alternatives/alternatives-metropolitan-secondary-1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-metropolitan-secondary-test.txt
-cat secondary/alternatives/alternatives-metropolitan-secondary-*.txt | fold -w 180 -s > $OUTPUT/alternatives-metropolitan-secondary.txt
+cat peripheral/primary/alternatives/alternatives--*.txt | fold -w 180 -s > $OUTPUT/alternatives-peripheral.txt
+cat peripheral/constructed/alternatives/alternatives--*.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-peripheral.txt
+cat metropolitan/constructed/alternatives/alternatives--*.txt | fold -w 180 -s > $OUTPUT/alternatives-constructed-metropolitan.txt
+cat metropolitan/primary/alternatives/alternatives-wss-*.txt | fold -w 180 -s > $OUTPUT/alternatives-wss-metropolitan.txt
+cat metropolitan/primary/alternatives/alternatives-spb-*.txt | fold -w 180 -s > $OUTPUT/alternatives-spb-metropolitan.txt
+cat metropolitan/primary/alternatives/alternatives-other-*.txt | fold -w 180 -s > $OUTPUT/alternatives-other-metropolitan.txt
+cat metropolitan/primary/alternatives/alternatives-wbo-*.txt | fold -w 180 -s > $OUTPUT/alternatives-wbo-metropolitan.txt
+
+cp -ruv metropolitan/secondary/alternatives/columns.txt $OUTPUT/columns-metropolitan-secondary.txt
+
+cat metropolitan/secondary/alternatives/alternatives--1_100.txt | fold -w 180 -s > $OUTPUT/alternatives-metropolitan-secondary-test.txt
+cat metropolitan/secondary/alternatives/alternatives--*.txt | fold -w 180 -s > $OUTPUT/alternatives-metropolitan-secondary.txt
