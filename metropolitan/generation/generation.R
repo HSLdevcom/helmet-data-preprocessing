89997bd909467b9f2d3d7d508781cdb1d73de2c5
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-09-05 14:42:13 +0300

    Fix order, subsetting, and dummies

--- metropolitan/generation/generation.R
+++ metropolitan/generation/generation.R
@@ -19,6 +19,7 @@ colnames(zones)[-1] = columns
 
 generation = load1(ancfile("primary/background.RData"))
 generation = leftjoin(generation, zones, by="rzone")
+generation = subset(generation, generation$rzone_capital_region | generation$rzone_surrounding_municipality)
 
 # ttypes_key.txt defines the class of each ttypes group. Groups not mentioned in
 # the file go into "muu / other" class.
@@ -37,6 +38,13 @@ generation$ttypes[m] = 1
 generation$homebased_tours_class = generation$homebased_tours
 m = which(generation$homebased_tours >= 4)
 generation$homebased_tours_class[m] = 4
+
+# Create dummy variables for accessibility measures
+generation$accessibility_work = 1.00
+generation$accessibility_study = 1.00
+generation$accessibility_school = 1.00
+generation$accessibility_shopping = 1.00
+generation$accessibility_other = 1.00
 
 # Check that all needed columns exist and order columns.
 columns = read.delims("order.txt")
