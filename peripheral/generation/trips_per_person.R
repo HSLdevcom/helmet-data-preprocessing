3170c39529f9332edd6925c40a3f7e490564b780
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-09-26 16:26:37 +0300

    Fix column name

--- peripheral/generation/trips_per_person.R
+++ peripheral/generation/trips_per_person.R
@@ -6,5 +6,5 @@ trips = load1("trips.RData")
 stat = fold(trips, .(ttype_peripheral),
             n=length(pid),
             xfactor=sum(xfactor))
-stat$xfactor = stat$xfactor / sum(background$xfactor)
+stat$xfactor_per_person = stat$xfactor / sum(background$xfactor)
 write.csv2(stat, file="trips_per_person.csv", row.names=FALSE)
