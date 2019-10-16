1082127a37fa547105528f1f925aef5577e0cad4
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-10-16 13:05:51 +0300

    Add column type file

--- survey/raw-heha.R
+++ survey/raw-heha.R
@@ -1,7 +1,7 @@
 # -*- coding: utf-8-unix -*-
 library(strafica)
 library(readxl)
-columns = read.delims("columns-heha.txt")
+columns = read.delims("coltypes-heha.txt")
 raw = read_xlsx(ancfile("input/HEHA-aineistot/MATKAT18_V3.xlsx"), col_types=columns$col_type)
 raw = as.data.frame(raw)
 save(raw, file="raw-heha.RData")
