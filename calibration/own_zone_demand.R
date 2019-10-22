636465796c0937e3b63d6316aa276b5f5c5c7360
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-10-22 15:55:25 +0300

    Fix own zone demand calculations

--- calibration/own_zone_demand.R
+++ calibration/own_zone_demand.R
@@ -6,23 +6,17 @@ tours = load1("tours.RData")
 model_types = read.delims("models.txt")
 modes = read.delims("modes.txt")
 zones = read.csv2(ancfile("area/zones.csv"))
-tours$internal = (tours$izone==tours$jzone)
+tours = subset(tours, izone==jzone)
+
 
 ###
 ### Internal and external tours
 ###
 
-internal = fold(tours, .(internal),
-                weight=sum(weight))
-
-internal_mode = fold(tours, .(internal, mode_name),
-                     weight=sum(weight))
-internal_mode = add_mode_share(internal_mode, internal)
-
-internal_model_type = fold(tours, .(internal, model_type),
+internal_model_type = fold(tours, .(model_type),
                            weight=sum(weight))
 
-internal_model_type_mode = fold(tours, .(internal, model_type, mode_name),
+internal_model_type_mode = fold(tours, .(model_type, mode_name),
                      weight=sum(weight))
 internal_model_type_mode = add_mode_share(internal_model_type_mode, internal_model_type)
 
@@ -30,18 +24,27 @@ internal_model_type_mode = add_mode_share(internal_model_type_mode, internal_mod
 ### Districts
 ###
 
-
-internal_district = fold(tours, .(internal, idistrict, jdistrict),
-                         weight=sum(weight))
-
-internal_district_mode = fold(tours, .(internal, idistrict, jdistrict, mode_name),
-                     weight=sum(weight))
-internal_district_mode = add_mode_share(internal_district_mode, internal_district)
-
-internal_district_model_type = fold(tours, .(internal, idistrict, jdistrict, model_type),
+internal_district_model_type = fold(tours, .(idistrict, jdistrict, model_type),
                            weight=sum(weight))
 
-internal_district_model_type_mode = fold(tours, .(internal, idistrict, jdistrict, model_type, mode_name),
+internal_district_model_type_mode = fold(tours, .(idistrict, jdistrict, model_type, mode_name),
                                 weight=sum(weight))
 internal_district_model_type_mode = add_mode_share(internal_district_model_type_mode, internal_district_model_type)
 
+stopifnot(all(internal_district_model_type_mode$idistrict==internal_district_model_type_mode$jdistrict))
+
+models = unique(model_types$model_type)
+mode_names = unique(modes$mode_name)
+for (i in seq_along(models)) {
+    output = subset(internal_district_model_type_mode,
+                    model_type %in% models[i])
+    stitle = sprintf("own_zone_demand-%s", models[i])
+    square = as_square_matrix(output,
+                              from="idistrict",
+                              to="mode_name",
+                              value="weight",
+                              from_names=unique(zones$district),
+                              to_names=mode_names,
+                              matrix_title=stitle)
+    write.delim(square, fname=sprintf("output/%s.txt", stitle))
+}
