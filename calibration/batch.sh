78316615ba54a5ffff3394d7bb97f5bde7712011
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-10-24 09:28:49 +0300

    Update batch.sh

--- calibration/batch.sh
+++ calibration/batch.sh
@@ -4,4 +4,4 @@ rm -fv *.csv
 Rscript --quiet --no-save --encoding=UTF-8 tours.R
 Rscript --quiet --no-save --encoding=UTF-8 demand.R
 Rscript --quiet --no-save --encoding=UTF-8 length.R
-Rscript --quiet --no-save --encoding=UTF-8 internal.R
+Rscript --quiet --no-save --encoding=UTF-8 own_zone_demand.R
