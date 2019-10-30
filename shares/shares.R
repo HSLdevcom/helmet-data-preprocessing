b94957d6d9db7b15d10a91cacc64aa0d68554851
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-10-30 09:46:59 +0200

    Fix naming

--- shares/shares.R
+++ shares/shares.R
@@ -8,16 +8,16 @@ peaks = list(morning=load1("peak_morning.RData"),
              afternoon=load1("peak_afternoon.RData"))
 
 temp = leftjoin(trips, peaks[["morning"]])
-m = which(with(temp, itime_trip >= lower & itime_trip < upper))
+m = which(with(temp, itime >= lower & itime < upper))
 share = sum(temp$xfactor[m] * temp$percentage[m]) / sum(temp$xfactor)
 print(share)
 
 temp = leftjoin(trips, peaks[["afternoon"]])
-m = which(with(temp, itime_trip >= lower & itime_trip < upper))
+m = which(with(temp, itime >= lower & itime < upper))
 share = sum(temp$xfactor[m] * temp$percentage[m]) / sum(temp$xfactor)
 print(share)
 
 temp = leftjoin(trips, peaks[["other"]])
-m = which(with(temp, itime_trip >= lower & itime_trip < upper))
+m = which(with(temp, itime >= lower & itime < upper))
 share = sum(temp$xfactor[m] * temp$percentage[m]) / sum(temp$xfactor)
 print(share)
