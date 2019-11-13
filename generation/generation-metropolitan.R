0186df5491d14cc1f596b4c5dc5868865f7dbc70
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-11-13 10:36:49 +0200

    Add age and car user group to metropolitan generation

--- generation/generation-metropolitan.R
+++ generation/generation-metropolitan.R
@@ -32,9 +32,30 @@ tours$weight = ifelse(tours$closed %in% 1, 1, 0.5) * tours$xfactor
 check.na(tours)
 
 background = load1(ancfile("metropolitan/primary/background.RData"))
-stat = fold(tours, .(model_type),
+background = subset(background,
+                    rzone_capital_region %in% 1 |
+                        rzone_surrounding_municipality %in% 1)
+background$age_group = NA
+m = which(background$age_7_17 %in% 1)
+background$age_group[m] = 1
+m = which(background$age_18_29 %in% 1)
+background$age_group[m] = 2
+m = which(background$age_30_49 %in% 1)
+background$age_group[m] = 3
+m = which(background$age_50_64 %in% 1)
+background$age_group[m] = 4
+m = which(background$age_65 %in% 1)
+background$age_group[m] = 5
+
+tours = leftjoin(tours, background, by="pid")
+
+stat = fold(tours, .(model_type, age_group, car_user),
             n=length(pid),
             weight=sum(weight))
-stat$weight_per_person = stat$weight / sum(background$xfactor)
+
+background = fold(background, .(age_group, car_user), xfactor=sum(xfactor))
+
+stat = leftjoin(stat, background)
+stat$weight_per_person = stat$weight / stat$xfactor
 write.csv2(stat, file="generation-metropolitan.csv", row.names=FALSE)
 print(stat)
