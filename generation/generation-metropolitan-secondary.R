3884fff90bbfd13445b81a9c81d7cf797aa7cf3e
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-12-18 11:57:21 +0200

    Calculate secondary generation by mode
    Also, age groups and car user groups are removed.

--- generation/generation-metropolitan-secondary.R
+++ generation/generation-metropolitan-secondary.R
@@ -22,11 +22,25 @@ observations$model_type[m] = "wo-secondary"
 m = which(observations$ttype %in% 7)
 observations$model_type[m] = "oo-secondary"
 
+# Modes
+observations$mode_name = NA
+m = which(observations$mode %in% 1)
+observations$mode_name[m] = "walk"
+m = which(observations$mode %in% 2)
+observations$mode_name[m] = "bike"
+m = which(observations$mode %in% 3)
+observations$mode_name[m] = "transit"
+m = which(observations$mode %in% c(4, 5))
+observations$mode_name[m] = "car"
+observations$mode_name = factor(observations$mode_name,
+                                levels=c("car","transit","bike","walk"))
+
 tours = pick(observations,
              pid,
              mode,
              xfactor,
              model_type,
+             mode_name,
              closed)
 tours$weight = ifelse(tours$closed %in% 1, 1, 0.5) * tours$xfactor
 check.na(tours)
@@ -35,27 +49,9 @@ background = load1(ancfile("metropolitan/primary/background.RData"))
 background = subset(background,
                     rzone_capital_region %in% 1 |
                         rzone_surrounding_municipality %in% 1)
-background$age_group = NA
-m = which(background$age_7_17 %in% 1)
-background$age_group[m] = 1
-m = which(background$age_18_29 %in% 1)
-background$age_group[m] = 2
-m = which(background$age_30_49 %in% 1)
-background$age_group[m] = 3
-m = which(background$age_50_64 %in% 1)
-background$age_group[m] = 4
-m = which(background$age_65 %in% 1)
-background$age_group[m] = 5
-
-tours = leftjoin(tours, background, by="pid")
-
-stat = fold(tours, .(model_type, age_group, car_user),
+stat = fold(tours, .(model_type, mode_name),
             n=length(pid),
             weight=sum(weight))
-
-background = fold(background, .(age_group, car_user), xfactor=sum(xfactor))
-
-stat = leftjoin(stat, background)
-stat$weight_per_person = stat$weight / stat$xfactor
+stat$weight_per_person = stat$weight / sum(background$xfactor)
 write.csv2(stat, file="generation-metropolitan-secondary.csv", row.names=FALSE)
 print(stat)
