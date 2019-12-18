f6b737dda98aefc7593a3f3ebe258f6bf787a7e7
Johanna Piipponen <Johanna.Piipponen@ramboll.fi>
2019-12-18 15:30:27 +0200

    Add a base_ttype to find out where oo trips start from

--- metropolitan/primary/observations.R
+++ metropolitan/primary/observations.R
@@ -19,6 +19,26 @@ observations$closed = ifelse(tours$closed, 1, 2)
 observations$order = tours$order
 observations$no_of_trips = tours$no_of_trips
 
+observations$base_ttype = NA
+m = which(observations$ttype %in% 1:5)
+observations$base_ttype[m] = observations$ttype[m]
+m = which(observations$ttype %in% 6)
+observations$base_ttype[m] = 1
+m = which(observations$ttype %in% 7)
+# Works only if the numbers are single-digit!
+previous_tour_type = sprintf("%d - %d", 1, as.integer(substr(tours$tour_type[m], 1, 1)))
+observations$base_ttype[m] = get_ttype(previous_tour_type,
+                                       rep(FALSE, times=length(previous_tour_type)))
+# Actually, if a non-home-based tour starts from business location, it should be
+# a work-based tour (ttype=6) with a base_ttype of 1. However, get_ttype does
+# not consider nhb tours starting from business locations to be wb tours. To
+# keep tour types the same and not to affect the estimation datas,
+# non-home-based tours (ttype=7) with a starting point in a business location
+# (base_ttype=1) are transformed into non-home-based trips (ttype=7) with a
+# starting point in other location (base_ttype=5).
+m = which(observations$ttype %in% 7 & observations$base_ttype %in% 1)
+observations$base_ttype[m] = 5
+
 m = match(tours$zone_origin, zones$zone_orig)
 observations$izone = zones$zone[m]
 observations$izone_cbd = ifelse(zones$cbd[m]==1, 1, 0)
